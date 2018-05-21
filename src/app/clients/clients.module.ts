36da92e871de5952b395e35a3cc151185a35769c
Scott Shillcock <scott@shillcock.net>
2018-05-21 08:46:27 -0700

    Remove reference to deleted components

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -4,15 +4,6 @@ import { SharedModule } from '@app/shared';
 import { ClientsRoutingModule } from './clients-routing.module';
 
 import { ClientComponent } from './client';
-import { ServiceComponent } from './service';
-
-/*
-import {
-  DefaultRendererComponent,
-  JsonRendererComponent,
-  CommandsRendererComponent
-} from './renderers';
-*/
 
 import { CommandsComponent } from './commands/commands.component';
 import { SubmitCommandDialogComponent } from './submit-command-dialog/submit-command-dialog.component';
@@ -22,19 +13,10 @@ import { ClientResolver } from './client.resolver';
   imports: [SharedModule, ClientsRoutingModule],
   declarations: [
     ClientComponent,
-    ServiceComponent,
-    // DefaultRendererComponent,
-    // JsonRendererComponent,
-    // CommandsRendererComponent,
     CommandsComponent,
     SubmitCommandDialogComponent
   ],
   providers: [ClientResolver],
-  entryComponents: [
-    SubmitCommandDialogComponent
-    // DefaultRendererComponent,
-    // JsonRendererComponent,
-    // CommandsRendererComponent
-  ]
+  entryComponents: [SubmitCommandDialogComponent]
 })
 export class ClientsModule {}
