d2bc021870d79b097a3ad02c0626e20918fa0581
Scott Shillcock <scott@shillcock.net>
2018-07-09 12:25:37 -0700

    Auto formatting

--- e2e/login-page.e2e-spec.ts
+++ e2e/login-page.e2e-spec.ts
@@ -8,33 +8,32 @@ describe('ASIS login', () => {
   beforeAll(() => {
     browser.waitForAngularEnabled(false);
   });
-  
-  beforeEach(() => {    
+
+  beforeEach(() => {
     page = new LoginPage();
   });
-  
+
   afterAll(() => {
     browser.waitForAngularEnabled(true);
   });
 
-  it('should get passed ASIS', async () => {    
+  it('should get passed ASIS', async () => {
     let loginPage = new LoginPage();
     // Navigate to Consent to Monitor Page
-    await loginPage.navigateToConsent();    
+    await loginPage.navigateToConsent();
     let url = await browser.getCurrentUrl();
-    expect(url).toContain('consent?continueToUrl=/gfmui/'); 
-    // CLick OK button to go to logon page   
+    expect(url).toContain('consent?continueToUrl=/gfmui/');
+    // CLick OK button to go to logon page
     const consentToMonitorButton = loginPage.getConsentToMonitorButton();
     await waitToBeVisible(consentToMonitorButton);
-    await consentToMonitorButton.click();     
+    await consentToMonitorButton.click();
     url = await browser.getCurrentUrl();
     expect(url).toContain('logon?continueToUrl=/gfmui/');
     // Click to login with CAC
     const loginWithCacButton = loginPage.getLoginWithCacButton();
     await waitToBeVisible(loginWithCacButton);
-    await loginWithCacButton.click();       
+    await loginWithCacButton.click();
     url = await browser.getCurrentUrl();
-    expect(url).toContain('gfmui/#/');    
+    expect(url).toContain('gfmui/#/');
   });
-
 });
--- e2e/pages/login.page.ts
+++ e2e/pages/login.page.ts
@@ -1,17 +1,15 @@
 import { browser, by, element } from 'protractor';
 
 export class LoginPage {
-  
   navigateToConsent() {
     return browser.get(`${browser.baseUrl}/consent?continueToUrl=/gfmui/`);
   }
-  
+
   getConsentToMonitorButton() {
     return element(by.buttonText('OK'));
   }
-  
+
   getLoginWithCacButton() {
     return element(by.buttonText('Continue'));
   }
-  
 }
