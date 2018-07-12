c21f3e57e8d31a307d0d7f9222d65721ebc13b29
Scott Shillcock <scott@shillcock.net>
2018-07-12 14:41:56 -0700

    Removed commented out code

--- e2e/login-page.e2e-spec.ts
+++ e2e/login-page.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { LoginPage } from './pages/login.page';
 import { browser } from 'protractor';
-// import { waitToBeVisible } from './helpers';
 
 describe('ASIS login', () => {
   let page: LoginPage;
@@ -19,20 +18,13 @@ describe('ASIS login', () => {
 
   it('should get passed ASIS', async () => {
     const loginPage = new LoginPage();
-
     await loginPage.navigateToConsent();
     let url = await browser.getCurrentUrl();
     expect(url).toContain('consent?continueToUrl=/gfmui/');
-
-    // await waitToBeVisible(loginPage.consentToMonitorButton);
     await loginPage.consentToMonitorButton.click();
-
     url = await browser.getCurrentUrl();
     expect(url).toContain('logon?continueToUrl=/gfmui/');
-
-    // await waitToBeVisible(loginPage.loginWithCacButton);
     await loginPage.loginWithCacButton.click();
-
     url = await browser.getCurrentUrl();
     expect(url).toContain('/gfmui/#/');
   });
